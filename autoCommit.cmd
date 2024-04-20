57112989d576013b6709cf5d92ada3c549c4a6d0
Davide Tagliafierro <davide.tagliafierro@gmail.com>
2024-04-20 18:29:17 +0200

    Auto update 20/04/2024 at 18:29:16,12

--- autoCommit.cmd
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-TIMESTAMP=$(date +%c)
-git add .
-git commit -m "Auto update $TIMESTAMP"
-git push origin main
--- autoCommit.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TIMESTAMP=$(date +%c)
-git add .
-git commit -m "Auto update $TIMESTAMP"
-git push origin main
-
-pause
